0f7ea2c8acc76970b03461a9b572a563a4a9d458
hecterbonha <untungsoandryanto@gmail.com>
2022-01-19 14:23:02 +0700

    feat(deploy): fix deplyment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,10 @@
 /* eslint-disable node/no-missing-import */
 import { deployMalahNgodingToken } from "./malahNgodingToken";
+import { deployNFT } from "./nft";
 
 async function main() {
   await deployMalahNgodingToken();
+  await deployNFT();
 }
 
 main().catch((error) => {
