f442f734b8019634cbdf3f60460394a7b51662a4
hecterbonha <untungsoandryanto@gmail.com>
2022-03-07 09:18:30 +0700

    chore(): mask token name

--- contracts/MalahNgodingToken.sol
+++ contracts/MalahNgodingToken.sol
@@ -6,7 +6,7 @@ import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
 contract MalahNgodingToken is ERC20 {
     uint256 private _totalSupply = 50000000;
 
-    constructor() ERC20("Malah Ngoding Token", "MNT") {
+    constructor() ERC20("b5ef6c8b39db0cd25f6f683a1425ec6f Token", "MNT") {
         _mint(msg.sender, _totalSupply * (10**decimals()));
     }
 }
