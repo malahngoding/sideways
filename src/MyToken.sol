e3c8f919d66cf0844af956505da9ee05aec38937
hecterbonha <untungsoandryanto@gmail.com>
2022-05-09 20:07:16 +0700

    feat(): foundry speed

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -1,58 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "openzeppelin-contracts/contracts/utils/Counters.sol";
+import "solmate/tokens/ERC721.sol";
+import "openzeppelin-contracts/contracts/utils/Strings.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract Polyland is ERC721, Ownable {
-    using Counters for Counters.Counter;
+contract NFT is ERC721 {
+    uint256 public currentTokenId;
 
-    Counters.Counter private supply;
+    constructor(string memory _name, string memory _symbol)
+        ERC721(_name, _symbol)
+    {}
 
-    uint256 public maxSupply = 4;
-
-    struct Triangle {
-        string name;
-        int8 edge1;
-        int8 edge2;
-        int8 edge3;
+    function mintTo(address recipient) public payable returns (uint256) {
+        uint256 newItemId = ++currentTokenId;
+        _safeMint(recipient, newItemId);
+        return newItemId;
     }
 
-    Triangle[] public triangles;
-
-    constructor() ERC721("Polyland", "PLLND") {
-        triangles.push(Triangle("Triangle0", 0, 0, 0));
-        triangles.push(Triangle("Triangle1", 1, 1, 1));
-        triangles.push(Triangle("Triangle2", 2, 2, 2));
-        triangles.push(Triangle("Triangle3", 3, 3, 3));
-        triangles.push(Triangle("Triangle4", 4, 4, 4));
-    }
-
-    modifier supplyCap() {
-        require(supply.current() <= maxSupply, "All patches minted.");
-        _;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return supply.current();
-    }
-
-    function getTriangles() public view returns (Triangle[] memory) {
-        return triangles;
-    }
-
-    function mintTriangle(address account)
+    function tokenURI(uint256 id)
         public
-        onlyOwner
-        supplyCap
-        returns (uint256)
+        view
+        virtual
+        override
+        returns (string memory)
     {
-        supply.increment();
-
-        uint256 newPatchId = supply.current();
-        _mint(account, newPatchId);
-
-        return newPatchId;
+        return Strings.toString(id);
     }
 }
